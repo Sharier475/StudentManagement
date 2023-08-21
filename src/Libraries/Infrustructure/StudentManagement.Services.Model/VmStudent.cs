3bdcfa328c2d775a227a9038c177f00177487684
Your Name <you@example.com>
2023-08-21 09:34:55 +0600

    Crud Complete

--- src/Libraries/Infrustructure/StudentManagement.Services.Model/VmStudent.cs
+++ src/Libraries/Infrustructure/StudentManagement.Services.Model/VmStudent.cs
@@ -1,25 +1,18 @@
 ﻿using StudentManagement.Shared.Common;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
-namespace StudentManagement.Services.Model
+namespace StudentManagement.Services.Model;
+
+public class VmStudent : IVm
 {
-    public class VmStudent : IVm
-    {
 
-        public string? StudentName { get; set; }
-        public double AdmissionFee { get; set; }
-        public string? CourseNamme { get; set; }
-        public int Id { get; set; }
-        public int TeacherId { get; set; }
-        [JsonIgnore]
+    public string? StudentName { get; set; }
+    public double AdmissionFee { get; set; }
+    public string? CourseNamme { get; set; }
+    public int Id { get; set; }
+    public int TeacherId { get; set; }
+    [JsonIgnore]
 
-        public VmTeacher? Teacher { get; set; }
-    }
+    public VmTeacher? Teacher { get; set; }
 }
 
--- src/Libraries/Infrustructure/StudentManagement.Services.Model/VmTeacher.cs
+++ src/Libraries/Infrustructure/StudentManagement.Services.Model/VmTeacher.cs
@@ -1,15 +1,14 @@
 ﻿using StudentManagement.Shared.Common;
 using System.Text.Json.Serialization;
 
-namespace StudentManagement.Services.Model
+namespace StudentManagement.Services.Model;
+
+public class VmTeacher : IVm
 {
-    public class VmTeacher : IVm
-    {
-        public int Id { get ; set ; }
-        public string? TeacherName { get; set; }
-        public double Salary { get; set; }
-        public string? Department { get; set; }
-        [JsonIgnore]
-        public ICollection<VmStudent> Students { get; set; } = new List<VmStudent>();
-    }
+    public int Id { get ; set ; }
+    public string? TeacherName { get; set; }
+    public double Salary { get; set; }
+    public string? Department { get; set; }
+    [JsonIgnore]
+    public ICollection<VmStudent> Students { get; set; } = new List<VmStudent>();
 }
